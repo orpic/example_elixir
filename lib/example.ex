d41151d05998eaf8e75b3df1a50f214f4974560b
Shobhit Umrao <shobhitumrao5@gmail.com>
2025-10-25 10:56:20 +0530

    starting point

--- lib/example.ex
+++ lib/example.ex
@@ -1,14 +1,12 @@
 defmodule Example do
   use Application
-  alias UUID
 
   def start(_type, _args) do
-    IO.puts(UUID.uuid4())
     Example.main()
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
   def main do
-    IO.puts("example")
+    IO.puts("example output")
   end
 end
