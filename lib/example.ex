0ef71df99b96ceca4d83c1c4da9f897843ba06bf
Shobhit Umrao <shobhitumrao5@gmail.com>
2025-10-25 14:19:50 +0530

    :rand , erlan modules

--- lib/example.ex
+++ lib/example.ex
@@ -15,22 +15,16 @@ defmodule Example do
   end
 
   def main do
-    gold_membership = %Membership{type: :gold, price: 25}
-    silver_membership = %Membership{type: :silver, price: 20}
-    platinum_membership = %Membership{type: :platinum, price: 15}
-    none_membership = %Membership{type: :none, price: 0}
+    coreect = :rand.uniform(11) - 1
+    IO.puts(coreect)
+    guess = IO.gets("Guess a number between 0 and 10: ") |> String.trim()
 
-    users = [
-      %User{name: "Shobhit", membership: gold_membership},
-      %User{name: "Sumit", membership: silver_membership},
-      %User{name: "NO NAME", membership: platinum_membership},
-      %User{name: "Ram", membership: none_membership}
-    ]
+    if String.to_integer(guess) == coreect do
+      IO.puts("You win")
+    else
+      IO.puts("You loose!")
+    end
 
-    Enum.each(users, fn %User{name: name, membership: membership} ->
-      IO.puts("#{name} has a #{membership.type} membership paying #{membership.price}")
-    end)
-
-    IO.puts("example output")
+    IO.puts("You guessed #{guess}, correct was #{coreect}")
   end
 end
